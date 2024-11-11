60b2931c29384e594c42cb97d0eb49071a9fc2f2
CCat <ccathlete01@gmail.com>
2024-11-11 05:27:10 +0100

    Added a contact page and a qa page + working with a custom router instead of handlerfunctions.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module webGo
 
 go 1.22.2
+
+require github.com/go-yaml/yaml v2.1.0+incompatible
+
+require (
+	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,75 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+
+	"github.com/go-yaml/yaml"
 )
 
-func handlerfunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Contact page</h1><p>To get in touch, email me at <a"+
+		"href=\"mailto:ccathlete01@gmail.com\">ccathlete01@gmail.com\n</a>.</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	// Creating a data bucket for the content.
+	var qaYaml struct {
+		questions string `yaml:"Content"`
+	}
+
+	// Opening the file for reading only.
+	file, err := os.Open("QA.yaml")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while opening internal QA file: %v", err), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	// Spilling the data into the bucket in small chunks.
+	if err := yaml.NewDecoder(file).Decode(&qaYaml); err != nil && err != io.EOF {
+		http.Error(w, fmt.Sprintf("Error while opening internal QA file: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>QA page</h1><p>%s</p>", qaYaml.questions)
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found.", http.StatusNotFound)
+
+	}
+
+	fmt.Fprintf(w, "\nThe current path is %s", r.URL.Path)
+}
+
+type Router struct{} // A simple implementation to the http.Handler
+// interface. It just needs to have the ServeHTTP method/
+
+func (Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	myHandler := pathHandler
+	myHandler(w, r)
+}
+
 func main() {
-	http.HandleFunc("/", handlerfunc)
+	var myRouter Router
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	// http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on: 3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", myRouter)
 }
