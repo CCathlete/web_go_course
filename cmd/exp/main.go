cc72e1752f5681a750e3128869665d59e266acb8
CCat <ccathlete01@gmail.com>
2024-11-11 16:22:57 +0100

    Started l32.

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -11,8 +11,9 @@ func main() {
 		panic(err)
 	}
 
-	user := struct{ Name string }{
+	user := struct{ Name, Bio string }{
 		Name: "Ken",
+		Bio:  "<script>alert(Haha you've been h4x0r3d!);</script>",
 	}
 
 	err = t.Execute(os.Stdout, user)
