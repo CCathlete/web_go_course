6b0c3aa92b6debbac07e613d2a6849039594516c
CCat <ccathlete01@gmail.com>
2024-11-21 16:03:52 +0100

    Added a footer and a header using a reusable layout.

--- views/templates.go
+++ views/templates.go
@@ -25,8 +25,8 @@ func ParseTemplate(templatePath string) (*Template, error) {
 	}, nil
 }
 
-func ParseFS(fs embed.FS, pattern string) (*Template, error) {
-	tpl, err := template.ParseFS(fs, pattern)
+func ParseFS(fs embed.FS, pattern ...string) (*Template, error) {
+	tpl, err := template.ParseFS(fs, pattern...)
 	if err != nil {
 		return nil, fmt.Errorf("error when parsing template: %w", err)
 	}
