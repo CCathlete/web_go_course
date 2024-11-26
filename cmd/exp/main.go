6e067cc4a7c38114e0deb04a460774153a1a0b62
CCat <ccathlete01@gmail.com>
2024-11-26 05:37:01 +0100

    Added code for adding users into the users table.

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -20,6 +20,55 @@ func main() {
 		panic(fmt.Sprintf("Error when pinging db: %v", err))
 	}
 	fmt.Println("Connected!")
+
+	// Creating a table.
+	_, err = db.Exec(`
+		create table if not exists users (
+			id serial primary key,
+			name text,
+			email text unique not null
+		);
+
+		create table if not exists orders (
+			id serial primary key,
+			user_id int not null,
+			amount int,
+			description text
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Tables created.")
+
+	// Insert some data.
+	usersDetails := []struct{ name, email string }{
+		{name: "Ken Cat", email: "ccat@example.com"},
+		{name: "Jon Calhoun", email: "JC@example.com"},
+		{name: "Missy Elliott", email: "doitdoitdoitdoit@example.com"},
+		{name: "Aero Smith", email: "dreamon@example.com"},
+	}
+
+	for _, user := range usersDetails {
+		_, err = db.Exec(`
+		insert into users (name, email) values
+		($1, $2);
+		`, user.name, user.email)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("Values inserted.")
+
+	_, err = db.Exec(`
+		select * from users;
+		-- drop table users;
+	`)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func connectionString() string {
