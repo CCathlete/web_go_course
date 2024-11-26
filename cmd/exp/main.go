0cd0914ac51a8a642a09826ccf70448fbecf37a1
CCat <ccathlete01@gmail.com>
2024-11-26 09:16:07 +0100

    Added code for returning a row from database + a nice error check using switch case.

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -15,60 +15,91 @@ func main() {
 	}
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		panic(fmt.Sprintf("Error when pinging db: %v", err))
-	}
-	fmt.Println("Connected!")
+	// err = db.Ping()
+	// if err != nil {
+	// 	panic(fmt.Sprintf("Error when pinging db: %v", err))
+	// }
+	// fmt.Println("Connected!")
 
-	// Creating a table.
-	_, err = db.Exec(`
-		create table if not exists users (
-			id serial primary key,
-			name text,
-			email text unique not null
-		);
+	// // Creating a table.
+	// _, err = db.Exec(`
+	// 	create table if not exists users (
+	// 		id serial primary key,
+	// 		name text,
+	// 		email text unique not null
+	// 	);
 
-		create table if not exists orders (
-			id serial primary key,
-			user_id int not null,
-			amount int,
-			description text
-		);
-	`)
-	if err != nil {
+	// 	create table if not exists orders (
+	// 		id serial primary key,
+	// 		user_id int not null,
+	// 		amount int,
+	// 		description text
+	// 	);
+	// `)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("Tables created.")
+
+	// // Insert some data.
+	// usersDetails := []struct{ name, email string }{
+	// 	{name: "Ken Cat", email: "ccat@example.com"},
+	// 	{name: "Jon Calhoun", email: "JC@example.com"},
+	// 	{name: "Missy Elliott", email: "doitdoitdoitdoit@example.com"},
+	// 	{name: "Aero Smith", email: "dreamon@example.com"},
+	// }
+
+	// for _, user := range usersDetails {
+	// 	row := db.QueryRow(`
+	// 	insert into users (name, email) values
+	// 	($1, $2) returning id, name;
+	// 	`, user.name, user.email)
+
+	// 	var id int
+	// 	var name string
+	// 	err = row.Scan(&id, &name)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	fmt.Println(id, name)
+	// 	// NOTE: This error check is useful only if we're not returning values from the query
+	// 	// because when we return values the scan would return an error if row is nil.
+	// 	// if err := row.Err(); err != nil {
+	// 	// 	panic(err)
+	// 	// }
+
+	// }
+
+	// fmt.Println("Values inserted.")
+
+	// _, err = db.Exec(`
+	// 	select * from users;
+	// 	-- drop table users;
+	// `)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	id := 4
+	var name, email string
+	row := db.QueryRow(`
+		select name, email
+		from users 
+		where id=$1;
+	`, id)
+
+	switch err := row.Scan(&name, &email); err {
+	case sql.ErrNoRows:
+		fmt.Printf("No matches were found: %v", err)
+	case nil:
+		break
+	default:
 		panic(err)
 	}
 
-	fmt.Println("Tables created.")
-
-	// Insert some data.
-	usersDetails := []struct{ name, email string }{
-		{name: "Ken Cat", email: "ccat@example.com"},
-		{name: "Jon Calhoun", email: "JC@example.com"},
-		{name: "Missy Elliott", email: "doitdoitdoitdoit@example.com"},
-		{name: "Aero Smith", email: "dreamon@example.com"},
-	}
-
-	for _, user := range usersDetails {
-		_, err = db.Exec(`
-		insert into users (name, email) values
-		($1, $2);
-		`, user.name, user.email)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	fmt.Println("Values inserted.")
-
-	_, err = db.Exec(`
-		select * from users;
-		-- drop table users;
-	`)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println(name, email)
 }
 
 func connectionString() string {
