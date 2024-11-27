14cf209909f5508c165f007a0c11537b3008e592
CCat <ccathlete01@gmail.com>
2024-11-27 04:16:44 +0100

    Created tweet, user and like types and some data.

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -3,84 +3,70 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"os"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-func main() {
+func sqlExs() {
 	db, err := sql.Open("pgx", connectionString())
 	if err != nil {
 		panic("Error when opening db.")
 	}
 	defer db.Close()
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	panic(fmt.Sprintf("Error when pinging db: %v", err))
-	// }
-	// fmt.Println("Connected!")
+	// Creating a table.
+	_, err = db.Exec(`
+		create table if not exists users (
+			id serial primary key,
+			username text unique not null
+		);
 
-	// // Creating a table.
-	// _, err = db.Exec(`
-	// 	create table if not exists users (
-	// 		id serial primary key,
-	// 		name text,
-	// 		email text unique not null
-	// 	);
+		create table if not exists tweets (
+			id serial primary key,
+			user_id int not null,
+			amount int,
+			description text
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
 
-	// 	create table if not exists orders (
-	// 		id serial primary key,
-	// 		user_id int not null,
-	// 		amount int,
-	// 		description text
-	// 	);
-	// `)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	fmt.Println("Tables created.")
 
-	// fmt.Println("Tables created.")
+	// Insert some data.
+	type user struct {
+		ID       int
+		username string
+	}
+	type tweet struct {
+		ID, userID int
+		content    string
+	}
+	type like struct {
+		ID, likerID, tweetID int
+	}
 
-	// // Insert some data.
-	// usersDetails := []struct{ name, email string }{
-	// 	{name: "Ken Cat", email: "ccat@example.com"},
-	// 	{name: "Jon Calhoun", email: "JC@example.com"},
-	// 	{name: "Missy Elliott", email: "doitdoitdoitdoit@example.com"},
-	// 	{name: "Aero Smith", email: "dreamon@example.com"},
-	// }
+	usersDetails := []user{
+		{username: "BilboB"},
+		{username: "FrodoB"},
+		{username: "SamoiseG"},
+		{username: "Mary"},
+		{username: "Pippin"},
+	}
 
-	// for _, user := range usersDetails {
-	// 	row := db.QueryRow(`
-	// 	insert into users (name, email) values
-	// 	($1, $2) returning id, name;
-	// 	`, user.name, user.email)
+	userTweets := []tweet{
+		{userID: 1, content: "Take the ring Frodo."},
+		{userID: 1, content: "I'm old."},
+		{userID: 1, content: "I'm retired."},
+	}
 
-	// 	var id int
-	// 	var name string
-	// 	err = row.Scan(&id, &name)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	// 	fmt.Println(id, name)
-	// 	// NOTE: This error check is useful only if we're not returning values from the query
-	// 	// because when we return values the scan would return an error if row is nil.
-	// 	// if err := row.Err(); err != nil {
-	// 	// 	panic(err)
-	// 	// }
-
-	// }
-
-	// fmt.Println("Values inserted.")
-
-	// _, err = db.Exec(`
-	// 	select * from users;
-	// 	-- drop table users;
-	// `)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	tweetLikes := []like{
+		{likerID: 2, tweetID: 1},
+		{likerID: 3, tweetID: 1},
+		{likerID: 4, tweetID: 1},
+		{likerID: 5, tweetID: 1},
+	}
 
 	// id := 4
 	// var name, email string
@@ -149,26 +135,6 @@ func main() {
 	fmt.Printf("The orders of user %d are: %v", userID, orders)
 }
 
-func connectionString() string {
-	PostgresConfig := struct {
-		Host     string
-		Port     string
-		User     string
-		Password string
-		DBName   string
-		SSLMode  string
-	}{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASS"),
-		DBName:   os.Getenv("DB_NAME"),
-		SSLMode:  os.Getenv("DB_SSL_MODE"),
-	}
-
-	return fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		PostgresConfig.Host, PostgresConfig.Port, PostgresConfig.User,
-		PostgresConfig.Password, PostgresConfig.DBName, PostgresConfig.SSLMode,
-	)
+func main() {
+	sqlExs()
 }
---
+++ cmd/exp/sql_lessons.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func sqlLessons() {
+	db, err := sql.Open("pgx", connectionString())
+	if err != nil {
+		panic("Error when opening db.")
+	}
+	defer db.Close()
+
+	// err = db.Ping()
+	// if err != nil {
+	// 	panic(fmt.Sprintf("Error when pinging db: %v", err))
+	// }
+	// fmt.Println("Connected!")
+
+	// // Creating a table.
+	// _, err = db.Exec(`
+	// 	create table if not exists users (
+	// 		id serial primary key,
+	// 		name text,
+	// 		email text unique not null
+	// 	);
+
+	// 	create table if not exists orders (
+	// 		id serial primary key,
+	// 		user_id int not null,
+	// 		amount int,
+	// 		description text
+	// 	);
+	// `)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("Tables created.")
+
+	// // Insert some data.
+	// usersDetails := []struct{ name, email string }{
+	// 	{name: "Ken Cat", email: "ccat@example.com"},
+	// 	{name: "Jon Calhoun", email: "JC@example.com"},
+	// 	{name: "Missy Elliott", email: "doitdoitdoitdoit@example.com"},
+	// 	{name: "Aero Smith", email: "dreamon@example.com"},
+	// }
+
+	// for _, user := range usersDetails {
+	// 	row := db.QueryRow(`
+	// 	insert into users (name, email) values
+	// 	($1, $2) returning id, name;
+	// 	`, user.name, user.email)
+
+	// 	var id int
+	// 	var name string
+	// 	err = row.Scan(&id, &name)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	fmt.Println(id, name)
+	// 	// NOTE: This error check is useful only if we're not returning values from the query
+	// 	// because when we return values the scan would return an error if row is nil.
+	// 	// if err := row.Err(); err != nil {
+	// 	// 	panic(err)
+	// 	// }
+
+	// }
+
+	// fmt.Println("Values inserted.")
+
+	// _, err = db.Exec(`
+	// 	select * from users;
+	// 	-- drop table users;
+	// `)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// id := 4
+	// var name, email string
+	// row := db.QueryRow(`
+	// select name, email
+	// from users
+	// where id=$1;
+	// `, id)
+	//
+	// switch err := row.Scan(&name, &email); err {
+	// case sql.ErrNoRows:
+	// fmt.Printf("No matches were found: %v", err)
+	// case nil:
+	// break
+	// default:
+	// panic(err)
+	// }
+	//
+	// fmt.Println(name, email)
+
+	// Creating orders for later.
+	// userID := 4
+	// for i := 1; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake order %d", amount)
+	// 	_, err := db.Exec(`insert into orders
+	// 	(user_id, amount, description)
+	// 	values ($1, $2, $3)`, userID, amount, desc,
+	// 	)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+	type Order struct {
+		ID, userID, amount int
+		description        string
+	}
+	var orders []Order
+
+	userID := 4
+	rows, err := db.Query(`
+	select id, amount, description
+	from orders
+	where user_id=$1;
+	`, userID)
+
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.userID = userID
+		err := rows.Scan(&order.ID, &order.amount, &order.description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	if err := rows.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("The orders of user %d are: %v", userID, orders)
+}
+
+func connectionString() string {
+	PostgresConfig := struct {
+		Host     string
+		Port     string
+		User     string
+		Password string
+		DBName   string
+		SSLMode  string
+	}{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSL_MODE"),
+	}
+
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		PostgresConfig.Host, PostgresConfig.Port, PostgresConfig.User,
+		PostgresConfig.Password, PostgresConfig.DBName, PostgresConfig.SSLMode,
+	)
+}
