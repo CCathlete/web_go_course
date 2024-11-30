419f5d0d273a4f34857b044e9d4965d43b4a1a59
CCat <ccathlete01@gmail.com>
2024-11-30 09:57:49 +0100

    Created a CSRF middleware to wrap the http router before activation.

--- cmd/routing/main.go
+++ cmd/routing/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"net/http"
 	"webGo/controllers"
+	"webGo/views"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/csrf"
 )
 
 func main() {
@@ -13,5 +16,18 @@ func main() {
 	// Set the routes in the router object.
 	controllers.RouteAll(myRouter)
 	fmt.Println("Starting the server on: 3000...")
-	http.ListenAndServe(":3000", myRouter)
+	csrfMW := prepMiddleWare(false) //TODO: change this before deploying.
+	http.ListenAndServe(":3000", csrfMW(myRouter))
+}
+
+// Creats a CSRF key and returns a middleware wrapper for
+// an http router (http.Handler)
+func prepMiddleWare(activate bool) func(http.Handler) http.Handler {
+	csrfKey := make([]byte, 32)
+	views.Must(rand.Read(csrfKey)) // Reads the content of a random num generator and writes it into the underlying array of the byteslice.
+	csrfMW := csrf.Protect(
+		csrfKey,
+		csrf.Secure(activate),
+	)
+	return csrfMW
 }
